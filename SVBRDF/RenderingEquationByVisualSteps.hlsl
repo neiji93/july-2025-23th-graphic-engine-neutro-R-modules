5453612effcdf3e7b4aca87985358b4677694f9b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-23 12:33:18 +0200

    Create RenderingEquationByVisualSteps.hlsl

---
+++ SVBRDF/RenderingEquationByVisualSteps.hlsl
@@ -0,0 +1,16 @@
+//we used a modified navier stokes equations for fluids.
+//The intuitiveness of the expression lead to simple analyses.
+// \vec{\nabla}\ - \vec{nabla}\ times \vec{q} - \vec{T}
+//Then the intensity of incoming rays does not reflect the real behaviour of materials.
+//This is not pbr.
+//I used heat transfer.
+
+void computeAlbedoNotPBR(...)
+{
+
+}
+
+void computeAlbedoForSVBRDFNotPBR(...)
+{
+
+}
