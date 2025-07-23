05d5980647e20c0163163c9c6ea351d70e9eead9
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-23 10:40:24 +0200

    Create SVBRDFDualMomentum.hlsl

---
+++ SVBRDF/SVBRDFDualMomentum.hlsl
@@ -0,0 +1,5 @@
+//a more generalized approach can be sighted. It means a generalized appraoch of main functions here
+//could prevent dissmiss inner properties of SVBDRF models resulting equations. Then, let s add functions to the main models.
+//In fact, it means removing artifacts in the old knowledge of textures. At first, we mixed 2 textures. But here, 
+//we will then add functions for two textures. Resulting in a different rendering equation. Notice that it could be done with SVBRDF functions.
+//We can use my scipy repository in forked with WKB support.
